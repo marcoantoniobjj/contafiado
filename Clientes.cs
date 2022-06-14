f41a9391bede6de83003faa9f572ef3c8434f0aa
Marco Antonio <marcoantonio.74ro@gmail.com>
2022-06-14 19:17:03 -0300

    Arquivos da primeira versão

---
+++ Clientes.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaFiado.Model
+{
+    public class Clientes
+    {
+        string _id;
+        public string Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        string _cpf;
+        public string CPF
+        {
+            get { return _cpf; }
+            set { _cpf = value; }
+        }
+
+
+        string _nome;
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+
+        string _logradouro;
+        public string Logradouro 
+        {
+            get { return _logradouro; }
+            set { _logradouro = value; }
+        }
+
+        string _complemento;
+        public string Complemento
+        {
+            get { return _complemento; }
+            set { _complemento = value; }
+        }
+
+        string _bairro;
+        public string Bairro
+        {
+            get { return _bairro; }
+            set { _bairro = value; }
+        }
+
+        string _cidade;
+        public string Cidade
+        {
+            get { return _cidade; }
+            set { _cidade = value; }
+        }
+
+        string _estado;
+        public string Estado
+        {
+            get { return _estado; }
+            set { _estado = value; }
+        }
+
+        bool _maiorIdade;
+        public bool MaiorIdade
+        {
+            get { return _maiorIdade; }
+            set { _maiorIdade = value; }
+        }
---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using ContaFiado.Model;
+
+namespace ContaFiado    
+{
+    class Program
+    {
+        static void Main()
+
+        {
+
+
+
+            Console.WriteLine("OPÇÕES");
+            Console.WriteLine("1 - Cadastro de Clientes");
+            Console.WriteLine("2 - Cadastro de Mercadoria");
+            Console.WriteLine("3 - Conta Fiado");
+            string opcao = Console.ReadLine();
+
+            if (opcao == "1")
+            {
+                //int IdadeAluno = 1;
+                //do
+                //{
+
+                //    Console.Clear();
+                //    Console.WriteLine("*** Cadastro de Clientes ***");
+
+            //        // Instanciando uma classe
+            //        Clientes pess01 = new Clientes { Id = Guid.NewGuid().ToString() };
+            //        //Aluno pess01 = new Aluno();
+            //        pess01.Id = Guid.NewGuid().ToString();
+
+            //        //atribuindo valor ao nome
+            //        Console.WriteLine("Digite o nome: ");
+            //        pess01.Nome = Console.ReadLine();
+
+            //        //atribuindo valor a data de nascimento
+            //        Console.WriteLine("Digite a data de nascimento: ");
+            //        pess01.DataNascimento = DateTime.Parse(Console.ReadLine());
+
+            //        //Calculando idade
+            //        IdadeAluno = DateTime.Now.Year - pess01.DataNascimento.Year;
+            //        if (DateTime.Now.DayOfYear < pess01.DataNascimento.DayOfYear) IdadeAluno--;
+
+            //        //atribuindo atribuindo valor a idade
+            //        //Console.WriteLine("Digite a idade: ");
+            //        //pess01.Idade = Int32.Parse(Console.ReadLine());
+            //        pess01.Idade = IdadeAluno;
+            //        Console.WriteLine(IdadeAluno + " anos de idade.");
+
+            //        //atribuindo valor a maior idade
+            //        if (IdadeAluno < 18)
+            //        {
+            //            Console.Clear();
+            //            Console.WriteLine("***Cadastro invalido para MENOR DE IDADE***");
+            //            Console.ReadKey();
+            //        }
+            //        else
+            //        {
+            //            //atribuindo valor ao peso
+            //            Console.WriteLine("Digite o peso: ");
+            //            pess01.Peso = Decimal.Parse(Console.ReadLine());
+
+            //        }
+
+            //    } while (IdadeAluno < 18);
+
+
+            //    //expondo os valores com tratamento de string
+            //    //Console.WriteLine("esse é o nome {0} essa aqui é a idade {1}",pess01.Nome,pess01.Idade);
+
+            //    //expondo todos os valores
+
+            //    //Console.WriteLine("Id: " + pess01.Id);
+            //    //Console.WriteLine("Nome: " + pess01.Nome);
+            //    //Console.WriteLine("Data de nascimento: " + pess01.DataNascimento);
+            //    //Console.WriteLine("Idade: " + pess01.Idade);
+            //    //Console.WriteLine("Peso: " + pess01.Peso);
+            //    //Console.WriteLine("Maioridade :" + pess01.MaiorIdade);
+
+            //}
+            //
+
+
+            if (opcao == "2")
+            {
+                Console.WriteLine("Hello opcao 2");
+
+            }
+
+
+        }
+    }
+}
